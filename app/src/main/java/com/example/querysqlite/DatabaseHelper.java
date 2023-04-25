9fcea920c944e12a0bb0e8d0eaaea8b904672ac2
hobaoan <20520876@gm.uit.edu.vn>
2023-04-25 20:19:59 +0700

    Set up

---
+++ app/src/main/java/com/example/querysqlite/DatabaseHelper.java
@@ -0,0 +1,11 @@
+package com.example.querysqlite;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+}
---
+++ app/src/main/java/com/example/querysqlite/ListInforView.java
@@ -0,0 +1,14 @@
+package com.example.querysqlite;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListInforView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+    }
+}
